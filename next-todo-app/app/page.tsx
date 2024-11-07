c68d73e8ffec678581e27308c39a65f91792be0a
Anpleenko Alexander <avanpleenko@gmail.com>
2024-11-07 18:41:05 +0300

    run prettier

--- next-todo-app/app/page.tsx
+++ next-todo-app/app/page.tsx
@@ -1,127 +1,145 @@
-"use client"
-import {CheckIcon, TrashIcon, Cross2Icon} from "@radix-ui/react-icons";
+'use client'
+import { CheckIcon, TrashIcon, Cross2Icon } from '@radix-ui/react-icons'
 
-import TodoForm from "@/components/todo/TodoForm";
-import {useEffect, useState} from "react";
-import {getTodos, deleteTodo, completeTodo, incompleteTodo} from "@/actions/todo";
-import TodoEditForm from "@/components/todo/TodoEditForm";
-
+import TodoForm from '@/components/todo/TodoForm'
+import { useEffect, useState } from 'react'
+import {
+  getTodos,
+  deleteTodo,
+  completeTodo,
+  incompleteTodo,
+} from '@/actions/todo'
+import TodoEditForm from '@/components/todo/TodoEditForm'
 
 export default function Home() {
-    const [todos, setTodos] = useState<any>(null)
-    useEffect(() => {
-        const getData = async () => {
-            const data = await getTodos()
-            setTodos(data)
-        }
-        getData()
-    }, []);
-
-    const handleCreateTodo = async () => {
-        const data = await getTodos()
-        setTodos(data)
+  const [todos, setTodos] = useState<any>(null)
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getTodos()
+      setTodos(data)
     }
+    getData()
+  }, [])
 
-    const incompletedTodos = (todo: any) => {
-        if (!todo.completed) {
-            return todo
-        }
+  const handleCreateTodo = async () => {
+    const data = await getTodos()
+    setTodos(data)
+  }
+
+  const incompletedTodos = (todo: any) => {
+    if (!todo.completed) {
+      return todo
     }
+  }
 
-    const completedTodos = (todo: any) => {
-        if (todo.completed) {
-            return todo
-        }
+  const completedTodos = (todo: any) => {
+    if (todo.completed) {
+      return todo
     }
+  }
 
-    const deleteSelectedTodo = (id: string) => {
-        deleteTodo(id).then(data => handleCreateTodo());
-    }
+  const deleteSelectedTodo = (id: string) => {
+    deleteTodo(id).then((data) => handleCreateTodo())
+  }
 
-    const completeSelectedTodo = (id: string) => {
-        completeTodo(id).then(data => handleCreateTodo())
-    }
+  const completeSelectedTodo = (id: string) => {
+    completeTodo(id).then((data) => handleCreateTodo())
+  }
 
-    const incompleteSelectedTodo = (id: string) => {
-        incompleteTodo(id).then(data => handleCreateTodo())
-    }
+  const incompleteSelectedTodo = (id: string) => {
+    incompleteTodo(id).then((data) => handleCreateTodo())
+  }
 
-    return (
-        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10 bg-slate-50 space-y-2">
-            {/* add logout button here */}
-                <div className="flex justify-end">
-                    <a href="http://localhost/oauth2/sign_out?rd=http%3A%2F%2Flocalhost%3A3000%2F"><p
-                        className="text-blue-500 underline transition cursor-pointer">Logout</p></a>
-                </div>
-            {/* logout button end */}
-            <h1 className="text-center text-2xl sm:text-3xl md:text-4xl font-bold text-slate-600 my-8">📝 Simple Todo
-                Service</h1>
-            <TodoForm onCreateTodo={handleCreateTodo}/>
-            <div className="space-y-2">
-                {
-                    todos?.filter(incompletedTodos).map((todo: any, index: number) => (
-                        <div
-                            className="flex items-center justify-between px-5 py-3 bg-white shadow-md rounded-full"
-                            key={index + 1}>
-                            <p className="text-slate-500 truncate max-w-[190px] md:max-w-[500px] md:text-lg">{todo.title}</p>
-                            <div className="space-x-2 flex items-center">
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10 bg-slate-50 space-y-2">
+      {/* add logout button here */}
+      <div className="flex justify-end">
+        <a href="http://localhost/oauth2/sign_out?rd=http%3A%2F%2Flocalhost%3A3000%2F">
+          <p className="text-blue-500 underline transition cursor-pointer">
+            Logout
+          </p>
+        </a>
+      </div>
+      {/* logout button end */}
+      <h1 className="text-center text-2xl sm:text-3xl md:text-4xl font-bold text-slate-600 my-8">
+        📝 Simple Todo Service
+      </h1>
+      <TodoForm onCreateTodo={handleCreateTodo} />
+      <div className="space-y-2">
+        {todos?.filter(incompletedTodos).map((todo: any, index: number) => (
+          <div
+            className="flex items-center justify-between px-5 py-3 bg-white shadow-md rounded-full"
+            key={index + 1}
+          >
+            <p className="text-slate-500 truncate max-w-[190px] md:max-w-[500px] md:text-lg">
+              {todo.title}
+            </p>
+            <div className="space-x-2 flex items-center">
+              <div
+                onClick={() => completeSelectedTodo(todo?._id)}
+                className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-green-400 hover:bg-green-400 transition"
+              >
+                <CheckIcon className="w-6 h-6 " />
+              </div>
 
-                                <div
-                                    onClick={() => completeSelectedTodo(todo?._id)}
-                                    className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-green-400 hover:bg-green-400 transition">
-                                    <CheckIcon className="w-6 h-6 "/>
-                                </div>
-
-                                <TodoEditForm onCreateTodo={handleCreateTodo} todoId={todo?._id}
-                                              title={todo?.title} description={todo?.description}/>
-                                <div
-                                    onClick={() => deleteSelectedTodo(todo?._id)}
-                                    className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-red-400 hover:bg-red-400 transition">
-                                    <TrashIcon className="w-6 h-6 "/>
-                                </div>
-                            </div>
-                        </div>
-
-                    ))
-                }
+              <TodoEditForm
+                onCreateTodo={handleCreateTodo}
+                todoId={todo?._id}
+                title={todo?.title}
+                description={todo?.description}
+              />
+              <div
+                onClick={() => deleteSelectedTodo(todo?._id)}
+                className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-red-400 hover:bg-red-400 transition"
+              >
+                <TrashIcon className="w-6 h-6 " />
+              </div>
             </div>
-            <div className="relative">
-                <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                    <div className="w-full border-t border-gray-300"/>
-                </div>
-                <div className="relative flex justify-center">
-                    <span className="bg-white px-2 my-10 text-sm text-gray-500"></span>
-                </div>
+          </div>
+        ))}
+      </div>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="bg-white px-2 my-10 text-sm text-gray-500"></span>
+        </div>
+      </div>
+      <div className="space-y-2">
+        {todos?.filter(completedTodos).map((todo: any, index: number) => (
+          <div
+            className="flex items-center justify-between px-5 py-3 bg-white shadow-md rounded-full"
+            key={index + 1}
+          >
+            <p className="text-slate-500 truncate max-w-[190px] md:max-w-[500px] md:text-lg line-through">
+              {todo.title}
+            </p>
+            <div className="space-x-2 flex items-center">
+              <div
+                onClick={() => incompleteSelectedTodo(todo?._id)}
+                className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-orange-400 hover:bg-orange-400 transition"
+              >
+                <Cross2Icon className="w-6 h-6 " />
+              </div>
+
+              <TodoEditForm
+                onCreateTodo={handleCreateTodo}
+                todoId={todo?._id}
+                title={todo?.title}
+                description={todo?.description}
+              />
+
+              <div
+                onClick={() => deleteSelectedTodo(todo?._id)}
+                className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-red-400 hover:bg-red-400 transition"
+              >
+                <TrashIcon className="w-6 h-6 " />
+              </div>
             </div>
-            <div className="space-y-2">
-                {
-                    todos?.filter(completedTodos).map((todo: any, index: number) => (
-                        <div
-                            className="flex items-center justify-between px-5 py-3 bg-white shadow-md rounded-full"
-                            key={index + 1}>
-                            <p className="text-slate-500 truncate max-w-[190px] md:max-w-[500px] md:text-lg line-through">{todo.title}</p>
-                            <div className="space-x-2 flex items-center">
-
-                                <div
-                                    onClick={() => incompleteSelectedTodo(todo?._id)}
-                                    className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-orange-400 hover:bg-orange-400 transition">
-                                    <Cross2Icon className="w-6 h-6 "/>
-                                </div>
-
-                                <TodoEditForm onCreateTodo={handleCreateTodo} todoId={todo?._id}
-                                              title={todo?.title} description={todo?.description}/>
-
-                                <div
-                                    onClick={() => deleteSelectedTodo(todo?._id)}
-                                    className="w-8 h-8 flex cursor-pointer justify-center text-slate-200 hover:text-white items-center rounded-full bg-white border-2 border-slate-200 hover:border-red-400 hover:bg-red-400 transition">
-                                    <TrashIcon className="w-6 h-6 "/>
-                                </div>
-
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-        </main>
-    );
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
--- next-todo-app/middleware.ts
+++ next-todo-app/middleware.ts
@@ -1,16 +1,15 @@
-import {NextRequest} from "next/server";
+import { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-    const oauthCookie = request.cookies.get('_oauth2_proxy')
+  const oauthCookie = request.cookies.get('_oauth2_proxy')
 
-    console.log('Current path', request.url)
+  console.log('Current path', request.url)
 
-
-    if (!oauthCookie && request.nextUrl.pathname !== '/login') {
-        return Response.redirect(new URL('/login', request.url));
-    }
+  if (!oauthCookie && request.nextUrl.pathname !== '/login') {
+    return Response.redirect(new URL('/login', request.url))
+  }
 }
 
 export const config = {
-    matcher: ['/', '/headers', '/login']
+  matcher: ['/', '/headers', '/login'],
 }
